1e8f136132aa246c0c03d7596ecd6e54b1a9eb5f
rishi-optiminastic <tech1@carboncut.co>
2026-03-05 14:54:30 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['https://rankking-be.onrender.com','anking-fe-chi.vercel.app','api.signalor.ai'] 
+ALLOWED_HOSTS = ['https://rankking-be.onrender.com','anking-fe-chi.vercel.app','api.signalor.ai','https://api.signalor.ai/'] 
 
 INSTALLED_APPS = [
     'django.contrib.admin',
